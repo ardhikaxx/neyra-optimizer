08334ec752c78c95c3a51bdfe837daabb0285914
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:51 +0700

    feat(app): penambahan kode penunjang halaman pengelolaan aplikasi startup

---
+++ src/NeyraOptimizer.App/Views/Pages/StartupPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class StartupPage : UserControl
+{
+    public StartupPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new StartupViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
