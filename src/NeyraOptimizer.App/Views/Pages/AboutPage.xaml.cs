4fceafbb4d17c3e55c395c39ce3c7169893568a5
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:01:10 +0700

    feat(app): penambahan kode penunjang halaman informasi tentang aplikasi

---
+++ src/NeyraOptimizer.App/Views/Pages/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class AboutPage : UserControl
+{
+    public string VersionText =>
+        string.Format(NeyraOptimizer.App.Localization.Translator.Instance["About.Version"],
+            System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version?.ToString(3) ?? "1.0.0");
+
+    public AboutPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+}
