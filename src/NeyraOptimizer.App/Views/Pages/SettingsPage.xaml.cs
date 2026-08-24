d63d298a1d540c759ccb132a08cdc0ae0c016f11
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:04:04 +0700

    feat(app): penambahan kode penunjang halaman pengaturan preferensi aplikasi

---
+++ src/NeyraOptimizer.App/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class SettingsPage : UserControl
+{
+    public SettingsPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new SettingsViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
