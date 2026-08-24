7ca7b4e9550a79bf8ae68151a55f5d3d1f451cd9
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:01:17 +0700

    feat(app): penambahan kode penunjang halaman analisis sistem

---
+++ src/NeyraOptimizer.App/Views/Pages/AnalyzePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.App.ViewModels;
+using NeyraOptimizer.Application.Session;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class AnalyzePage : UserControl
+{
+    public AnalyzePage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new AnalyzeViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
