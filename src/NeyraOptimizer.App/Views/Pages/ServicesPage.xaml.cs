5e1d75ada73546834201f28faa67860b109c7bf6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:45 +0700

    feat(app): penambahan kode penunjang halaman pengelolaan layanan Windows

---
+++ src/NeyraOptimizer.App/Views/Pages/ServicesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class ServicesPage : UserControl
+{
+    public ServicesPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new ServicesViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
