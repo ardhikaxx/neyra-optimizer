3413d49c30ff695c3327b9f8d95fd50f3b5abe8f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:33 +0700

    feat(app): penambahan kode penunjang halaman pusat pemulihan sistem

---
+++ src/NeyraOptimizer.App/Views/Pages/RestoreCenterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class RestoreCenterPage : UserControl
+{
+    public RestoreCenterPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new RestoreCenterViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
