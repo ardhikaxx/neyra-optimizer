922552fce9fa02d8ba20deea17107ac607f93d63
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:01:25 +0700

    feat(app): penambahan kode penunjang halaman aplikasi latar belakang

---
+++ src/NeyraOptimizer.App/Views/Pages/BackgroundAppsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.App.ViewModels;
+using NeyraOptimizer.Application.Session;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class BackgroundAppsPage : UserControl
+{
+    private readonly BackgroundAppsViewModel _vm;
+
+    public BackgroundAppsPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        _vm = new BackgroundAppsViewModel(sp.GetRequiredService<SessionState>(), sp);
+        DataContext = _vm;
+    }
+
+    private async void BgToggle_Changed(object sender, RoutedEventArgs e)
+    {
+        if ((sender as FrameworkElement)?.DataContext is ViewModels.BgAppToggleRow row)
+            await _vm.ToggleBackgroundAsync(row);
+    }
+}
