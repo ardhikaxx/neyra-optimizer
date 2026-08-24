63b4907f39d1bd62097754f27ead58d907bba393
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:26 +0700

    feat(app): penambahan kode penunjang halaman pengaturan privasi sistem

---
+++ src/NeyraOptimizer.App/Views/Pages/PrivacyPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.App.ViewModels;
+using NeyraOptimizer.Application.Session;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class PrivacyPage : UserControl
+{
+    private readonly PrivacyViewModel _vm;
+
+    public PrivacyPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        _vm = new PrivacyViewModel(sp.GetRequiredService<SessionState>(), sp);
+        DataContext = _vm;
+    }
+
+    private async void Privacy_Changed(object sender, RoutedEventArgs e)
+    {
+        if ((sender as FrameworkElement)?.DataContext is ViewModels.PrivacyRow row)
+            await _vm.ToggleAsync(row);
+    }
+}
