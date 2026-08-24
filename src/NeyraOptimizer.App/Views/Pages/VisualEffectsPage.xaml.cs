4954392f7e60bcd4a6506bc301f03fe01b2aaed3
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:57 +0700

    feat(app): penambahan kode penunjang halaman pengaturan efek visual

---
+++ src/NeyraOptimizer.App/Views/Pages/VisualEffectsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.App.ViewModels;
+using NeyraOptimizer.Application.Session;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class VisualEffectsPage : UserControl
+{
+    private readonly VisualEffectsViewModel _vm;
+
+    public VisualEffectsPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        _vm = new VisualEffectsViewModel(sp.GetRequiredService<SessionState>(), sp);
+        DataContext = _vm;
+    }
+
+    private async void Effect_Changed(object sender, RoutedEventArgs e)
+    {
+        if ((sender as FrameworkElement)?.DataContext is Domain.Models.Power.VisualEffectItem effect
+            && effect.ProposedEnabled != effect.CurrentEnabled)
+            await _vm.ToggleEffectAsync(effect);
+    }
+}
