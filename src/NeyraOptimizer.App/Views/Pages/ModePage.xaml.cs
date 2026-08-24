f9180d1e667a9e78f77380d4f9338ab3933f4b9f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:07 +0700

    feat(app): penambahan kode penunjang halaman profil mode optimasi

---
+++ src/NeyraOptimizer.App/Views/Pages/ModePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.App.ViewModels;
+using NeyraOptimizer.Application.Session;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+/// <summary>Hosts one usage mode (Gaming / Office / Battery Saver / Low-End / Safe Windows).</summary>
+public partial class ModePage : UserControl
+{
+    public ModePage(IServiceProvider sp, string modeKey)
+    {
+        InitializeComponent();
+        DataContext = new ModeViewModel(modeKey, sp.GetRequiredService<SessionState>(), sp);
+    }
+}
