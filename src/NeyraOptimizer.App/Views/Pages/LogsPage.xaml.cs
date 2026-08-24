f212cacf50363b70da9c31365160ede7d60b21bf
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:01 +0700

    feat(app): penambahan kode penunjang halaman penampil berkas log

---
+++ src/NeyraOptimizer.App/Views/Pages/LogsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class LogsPage : UserControl
+{
+    public LogsPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new LogsViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
