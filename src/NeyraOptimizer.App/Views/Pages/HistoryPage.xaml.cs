d86271643446a421912ca4ca7df0e87dcebafdb2
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:01:56 +0700

    feat(app): penambahan kode penunjang halaman riwayat tindakan optimasi

---
+++ src/NeyraOptimizer.App/Views/Pages/HistoryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class HistoryPage : UserControl
+{
+    public HistoryPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new HistoryViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
