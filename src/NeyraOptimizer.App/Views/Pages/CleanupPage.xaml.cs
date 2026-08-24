5763171881ae572ac56b1f797c75d65aabef8640
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:01:32 +0700

    feat(app): penambahan kode penunjang halaman pembersihan sistem

---
+++ src/NeyraOptimizer.App/Views/Pages/CleanupPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class CleanupPage : UserControl
+{
+    public CleanupPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new CleanupViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
