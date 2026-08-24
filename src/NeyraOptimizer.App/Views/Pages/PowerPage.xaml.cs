7e5d48837e15fe9552c6c49d3aeb9f49603d5651
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 16:02:20 +0700

    feat(app): penambahan kode penunjang halaman konfigurasi rencana daya

---
+++ src/NeyraOptimizer.App/Views/Pages/PowerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using NeyraOptimizer.Application.Session;
+using NeyraOptimizer.App.ViewModels;
+
+namespace NeyraOptimizer.App.Views.Pages;
+
+public partial class PowerPage : UserControl
+{
+    public PowerPage(IServiceProvider sp)
+    {
+        InitializeComponent();
+        DataContext = new PowerViewModel(sp.GetRequiredService<SessionState>(), sp);
+    }
+}
