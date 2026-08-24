b9152c4b750c84f98af29254bfbb41ef7d28c2c8
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-24 15:57:07 +0700

    feat(infrastructure): penambahan antarmuka pelacak operasi tertunda untuk pemulihan crash

---
+++ src/NeyraOptimizer.Infrastructure/CrashRecovery/IPendingOperationTracker.cs
@@ -0,0 +1,11 @@
+﻿using NeyraOptimizer.Domain.Snapshots;
+
+namespace NeyraOptimizer.Infrastructure.CrashRecovery;
+
+public interface IPendingOperationTracker
+{
+    void Begin(PendingOperationRecord record);
+    void UpdatePhase(Guid operationId, string phase, int completedChanges);
+    void Clear();
+    PendingOperationRecord? ReadPending();
+}
